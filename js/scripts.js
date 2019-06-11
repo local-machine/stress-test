cc618e3e09d448ec79a19a34c9c95f82157e09fe
Jessica Munoz and Rochelle Roberts <jessica@>
2019-06-11 16:26:23 -0700

    created webpage for stress test

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,48 @@
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
+  $("form#stress_survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+    $("#warning-responses").show();
+    var warningCount = 0;
+    var symptomCount= 0;
+    var strategyCount= 0;
+    $("input:checkbox[name=warning]:checked").each(function(){
+      warningCount +=1;
+      var warningMode = $(this).val();
+      $('#warning-responses').append(warningMode + "<br>");
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
+    $("#symptoms-responses").show();
+    $("input:checkbox[name=symptoms]:checked").each(function(){
+      symptomCount += 1;
+      var symptomsMode = $(this).val();
+      $('#symptoms-responses').append(symptomsMode + "<br>");
     });
-    $('#transportation_survey').hide();
+    $("#strategy-responses").show();
+    $("input:checkbox[name=strategy]:checked").each(function(){
+      strategyCount +=1;
+      var warningMode = $(this).val();
+      $('#strategy-responses').append(warningMode + "<br>");
+    });
+debugger;
+    if(warningCount >= 3 && symptomCount >= 3){
+      // you are stressed and we print stress info
+      $("#high-result").show();
+      $("#medium-result").hide();
+      $("#low-result").hide();
+
+    }else if( warningCount ===2 || warningCount === 1 && symptomCount === 2 || symptomCount === 1){
+      // you are good, but check out more stategies
+      $("#medium-result").show();
+      $("#high-result").hide();
+      $("#low-result").hide();
+    }else {
+      // no prob but be aware!
+      $("#low-result").show();
+      $("#medium-result").hide();
+      $("#high-result").hide();
+
+    }
+
+
+    $('#stress_survey').hide();
   });
 });
